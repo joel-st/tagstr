e0d1510822a5e92733f6c41a37b5113e491d7a17
Lone <17547231+joel-st@users.noreply.github.com>
2026-01-15 12:46:41 +0100

    workflow update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solid()],
-  server: {
-    port: 3000,
-    host: true
-  },
-  build: {
-    target: 'esnext'
-  }
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/tagstr/' : '/',
+    plugins: [solid()],
+    server: {
+      port: 3000,
+      host: true
+    },
+    build: {
+      target: 'esnext'
+    }
+  };
 });
